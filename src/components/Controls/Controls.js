e1d87b829b79a8a1a1a56c6ff51c9c4ada2cadeb
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2021-06-02 14:03:15 +0300

    fix: change position of controls

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -5,16 +5,13 @@ import './Controls.css';
 function Controls(props) {
   return(
     <nav className="controls">
-      {/* Time, Score */}
       <Button userClass="controls__btn">
         Start
       </Button>
       <Button userClass="controls__btn">
         Stop
       </Button>
-      <Button userClass="controls__btn">
-        Pause
-      </Button>
+      <span className="controls__time"/>
     </nav>
   );
 }
